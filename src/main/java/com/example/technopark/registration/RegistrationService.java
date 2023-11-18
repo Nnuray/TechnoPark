62b2de5b0d11c2b24b2d325acda1d350bd39a4c6
Nuray <nuray.kavkaeva.04@gmail.com>
2023-11-18 13:51:10 +0600

    registration service

--- src/main/java/com/example/technopark/registration/RegistrationService.java
+++ src/main/java/com/example/technopark/registration/RegistrationService.java
@@ -1,10 +1,30 @@
 package com.example.technopark.registration;
 
+import com.example.technopark.appuser.AppUser;
+import com.example.technopark.appuser.AppUserRole;
+import com.example.technopark.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+    private final AppUserService appUserService;
+    private final EmailValidator emailValidator;
     public String register(RegistrationRequest request) {
-        return "works";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+
+        if(!isValidEmail){
+            throw new IllegalStateException("email not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
